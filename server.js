62a703a787eca194bbb95f7adc45625673b5ce9f
Dhaval Patil <d22it209@charusat.edu.in>
2024-07-09 23:17:03 +0530

    Learned Counter

---
+++ server.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import morgan from 'morgan';
+import connectDB from './config/db.js';
+
+// configure
+dotenv.config()
+
+// database config
+connectDB();
+//rest Object
+const app = express();
+
+// middlewares
+app.use(express.json())
+app.use(morgan('dev'))
+
+//rest api
+app.get('/', (req, res) => {
+    res.send('<h1>Welcome to Server</h1>')
+})
+
+//port
+const PORT = process.env.PORT || 8080;
+
+//run listen
+app.listen(PORT, () => {
+    console.log(`Server Running on ${process.env.DEV_MODE} mode on PORT  ${PORT}`);
+})
