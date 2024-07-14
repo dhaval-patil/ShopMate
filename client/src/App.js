a01b458218ec84daaea7b26cec06304451c58053
Dhaval Patil <d22it209@charusat.edu.in>
2024-07-14 21:28:39 +0530

    LOGIN page

--- client/src/App.js
+++ client/src/App.js
@@ -5,18 +5,19 @@ import Contact from "./pages/Contact";
 import Policy from "./pages/Policy";
 import Pagenotfound from "./pages/Pagenotfound";
 import Register from "./pages/Auth/Register";
-import {ToastContainer} from 'react-toastify';
+import Login from "./pages/Auth/LogIn";
 function App() {
   return (
     <>
-    <Routes>
-      <Route path='/' element={<HomePage />} />
-      <Route path='/register' element={<Register />} />
-      <Route path='/about' element={<About />} />
-      <Route path='/contact' element={<Contact />} />
-      <Route path='/policy' element={<Policy />} />
-      <Route path='*' element={<Pagenotfound />} />
-    </Routes>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/policy" element={<Policy />} />
+        <Route path="*" element={<Pagenotfound />} />
+      </Routes>
     </>
   );
 }
