b3db066fd8450a19af568ae0198e4e0b3b0df68a
Dhaval Patil <d22it209@charusat.edu.in>
2024-09-29 00:38:12 +0530

    admin

--- client/src/components/Layout/AdminMenu.js
+++ client/src/components/Layout/AdminMenu.js
@@ -12,6 +12,7 @@ const AdminMenu = () => {
                     <NavLink to="/dashboard/admin/create-product" className="list-group-item list-group-item-action">Create Product</NavLink>
                     <NavLink to="/dashboard/admin/users" className="list-group-item list-group-item-action">Users</NavLink>
 
+
                 </div>
             </div>
 
